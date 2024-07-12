cc7b3823da1bb462dafa7668f81f598befe618b6
Gabriel <gabs1995@hotmail.com>
2024-07-11 21:07:31 -0500

    :white_check_mark: fix failing tests

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = Quote.ordered.first
   end
 
   test "Creating a new quote" do
@@ -12,17 +12,13 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
+    # we expect to stay here and render a "New quote" form
     click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
     fill_in "Name", with: "Capybara quote"
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
 
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
+    # We expect to see our "Capybara quote" added to the list
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
   end
@@ -39,9 +35,8 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
