b7f52b6f3f3c0c6072aa8a2c2a2d71a5a8afbeae
Gabriel <gabs1995@hotmail.com>
2024-07-28 14:24:16 -0500

    :sparkles: update total price with turbo streams

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,10 @@ module ApplicationHelper
   def nested_dom_id(*args)
     args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
   end
+
+  def render_turbo_stream_total_price_quote
+    turbo_stream.replace dom_id(@quote, :total_price) do
+      render "quotes/total", quote: @quote
+    end
+  end
 end
